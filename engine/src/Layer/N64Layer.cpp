47456506d984d317c1e633d2fe7817633bcbc758
TyS004 <76879612+TyS004@users.noreply.github.com>
2026-05-29 03:57:02 -0700

    LibDragon

---
+++ engine/src/Layer/N64Layer.cpp
@@ -0,0 +1,9 @@
+#include "Layer/N64Layer.h"
+
+E64::N64Layer::N64Layer(){
+    
+}
+
+E64::N64Layer::~N64Layer(){
+    
+}
---
+++ platform/N64/include/N64Renderer.h
@@ -0,0 +1,6 @@
+#ifndef N64_RENDERER_H
+#define N64_RENDERER_H
+
+
+
+#endif
